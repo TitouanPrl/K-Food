0ec1c16143df2dffdcaaf86a6fe695e5c1e6a7ad
TitouanPrl <titouan.pradal@gmail.com>
2023-03-31 10:07:11 +0200

    require -> header et session_start

--- php/categories.php
+++ php/categories.php
@@ -1,17 +1,21 @@
 <?php
+session_start();
+
+$cat = $_GET['cat'];
+
 /* A REFAIRE CAR BIG FRAUDE */
     /* Accès à la page plat */
-    if ($cat = 'plat') {
-        require 'plat.php';
+    if ($cat == 'plat') {
+        header("Location:plat.php");
     }
 
     /* Accès à la page dessert */
-    elseif ($cat = 'dessert') {
-        require 'dessert.php';
+    elseif ($cat == 'dessert') {
+        header("Location:dessert.php");
     }
 
     /* Accès à la page resto */
-    elseif ($cat = 'resto') {
-        require 'resto.php';
+    elseif ($cat == 'resto') {
+        header("Location:resto.php");
     }
 ?>
