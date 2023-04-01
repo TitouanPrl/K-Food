2e16abe4b8bf83bf4876bf32fb34db7aca77a25a
TitouanPrl <titouan.pradal@gmail.com>
2023-04-02 00:07:44 +0200

    update parcours fichier json

--- php/verifConnexion.php
+++ php/verifConnexion.php
@@ -18,7 +18,7 @@ $file = "../infos/user.json";
 $data = file_get_contents($file);
 
 /* On le décode */
-$obj = json_decode($data);
+$obj = json_decode($data,true);
 
 
 /* Si un fichier n'existe pas on renvoit une erreur */
@@ -28,22 +28,29 @@ if (!filesize($file)) {
 
 /* Sinon on vérifie que l'utilisateur existe dans le fichier */
 else {
-  foreach($obj['user'] as $current) {
+  foreach($obj['userList'] as $current) {
 
     /* On écrit les var dans la session */
     $_SESSION['login1'] = $current['userLogin'];
     $_SESSION['mdp1'] = $current['userMdp'];
 
-    /* Si les infos de connexion correspondent on redirige vers l'accueil */
+    /* Si les infos de connexion correspondent on passe l'état à connecté et on redirige vers l'accueil */
     if ($_SESSION['login'] == $_SESSION['login1'] && $_SESSION['mdp'] == $_SESSION['mdp1']){
+
+      //$current['connect'] = 'true'; A REFAIRE
+
+      //$updFile = fopen("../infos/user.json", "r+");
+      //fwrite($updFile, json_encode($obj));
+      //fclose($file);
+
       header('Location: index.php'); 
 
       exit();
     }
   }
 
-  /* On affiche une erreur si les données de connexions ne correspondent pas */
-  echo "Le login ou le mot de passe est incorrect";   
+  /* Si les données de connexions ne correspondent pas on renvoi vers la connexion */
+  session_destroy(); 
   header('Location: connexion.php');
 
 }
