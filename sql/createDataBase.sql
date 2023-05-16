4377f899696bea3da7ea9b2a6082849222c14744
TitouanPrl <titouan.pradal@gmail.com>
2023-05-16 14:17:04 +0200

    script sql pour créer la BDD

---
+++ sql/createDataBase.sql
@@ -0,0 +1,40 @@
+DROP DATABASE IF EXISTS KFood;
+CREATE DATABASE KFood;
+USE KFood;
+
+CREATE TABLE Client(
+    idClient INT PRIMARY KEY,
+    login VARCHAR(30),
+    mdp VARCHAR(30),
+    connect BOOLEAN
+);
+
+CREATE TABLE Plats(
+    img VARCHAR(40),
+    nom VARCHAR(30) PRIMARY KEY,
+    desc VARCHAR(200),
+    prix VARCHAR(4),
+    stock INT
+);
+
+CREATE TABLE Desserts(
+    img VARCHAR(40),
+    nom VARCHAR(30) PRIMARY KEY,
+    desc VARCHAR(200),
+    prix VARCHAR(4),
+    stock INT
+);
+
+CREATE TABLE Resto(
+    img VARCHAR(40),
+    nom VARCHAR(30) PRIMARY KEY,
+    desc VARCHAR(200),
+    prix VARCHAR(4),
+    stock INT
+);
+
+CREATE TABLE Panier(
+    nom VARCHAR(40),
+    prix VARCHAR(4),
+    quantite INT
+);
