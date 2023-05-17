0f93141cc2823b8020206c5968566cdfa8f4a035
TitouanPrl <titouan.pradal@gmail.com>
2023-05-17 18:09:53 +0200

    Cryptage des mdp

--- Archives/encode.php
+++ Archives/encode.php
@@ -1,10 +1,10 @@
 <?php 
 
-//echo password_hash("19", PASSWORD_DEFAULT);
-//echo password_hash("Coree", PASSWORD_DEFAULT);
-//echo password_hash("Paularis", PASSWORD_DEFAULT);
-//echo password_hash("Coupole", PASSWORD_DEFAULT);
-//echo password_hash("GoatSis", PASSWORD_DEFAULT);
+echo password_hash("19", PASSWORD_DEFAULT);
+echo password_hash("Coree", PASSWORD_DEFAULT);
+echo password_hash("Paularis", PASSWORD_DEFAULT);
+echo password_hash("Coupole", PASSWORD_DEFAULT);
+echo password_hash("GoatSis", PASSWORD_DEFAULT);
 
 /* 19 : $2y$10$aE68fuwppOIU/gmRDmlIOO929Q06bTA01IsjpnPSl6PjXyDteXzne
 Coree : $2y$10$8/1SqSr2QhwND5dYZCDKfOeRtqM6XzdavHnj16FC7zawOUFZO04nW
