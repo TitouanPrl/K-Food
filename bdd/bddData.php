e095cfe110e209db27c7faf7e19eef6bbc86326f
TitouanPrl <titouan.pradal@gmail.com>
2023-05-17 19:31:43 +0200

    login bdd root

--- bdd/bddData.php
+++ bdd/bddData.php
@@ -2,10 +2,10 @@
 
 /* Définition des variables de connexion */
 define("SERV", "localhost");
-//define("LOGIN", "root");
-//define("MDP", "");
-define("LOGIN", "devWeb");
-define("MDP", "devWeb31$");
+define("LOGIN", "root");
+define("MDP", "");
+//define("LOGIN", "devWeb");
+//define("MDP", "devWeb31$");
 define("BDD", "KFood");
 
 ?>
